5db176e0baeebec51efab83991552a313fd35923
rneiva <raulneivaweb@gmail.com>
2024-06-13 16:40:30 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/rneiva/go-oauth2/config"
+	"github.com/rneiva/go-oauth2/controllers"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Post("/google_login", controllers.GoogleLogin)
+	//app.Post("/google_callback", controllers.GoogleCallback)
+
+	app.Listen(":8000")
+}
